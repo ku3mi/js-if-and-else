1bd688301fdff52236f5f4b32ada5544bd857d07
ku3mi <opdif@mail.ru>
2022-09-17 21:25:07 +0300

    if and else

---
+++ start-js-if-else/js/script.js
@@ -0,0 +1,67 @@
+// if and else
+let message = "Alina";
+if (5 < 100) {
+   console.log(message);
+}
+// 
+let user = "Hello, how a you?"
+
+let a = 5;
+if (a > 10) {
+   console.log(user);
+} else {
+   console.log('You have no message :(');
+}
+
+// 
+
+let addUser = "Hello";
+let nickName
+if (5 > 1) {
+   nickName = (", Alina!");
+} else {
+   nickName = (", Mark!");
+}
+addUser += nickName;
+console.log(addUser);
+
+
+
+
+// короткая запись  ? -- if(если)   : --- else(иначе)
+let userName = "hello!!";
+let nickUser = (5 > 10) ? ", Alina" : ", Mark";
+userName += nickUser;
+console.log(userName);
+
+// Практика список имен
+
+let userNick = "Hello";
+let userMessage;
+
+if (5 > 50) {
+   userMessage = ", Alina";
+} else if (5 > 45) {
+   userMessage = ", Anna";
+} else if (5 > 35) {
+   userMessage = ", Lika";
+} else if (5 > 25) {
+   userMessage = ", Elena";
+} else if (5 > 20) {
+   usermessage = ", Egor";
+} else if (5 > 10) {
+   userMessage = ", Vika";
+} else if (5 > 9) {
+   userMessage = ", Atnon";
+} else {
+   userMessage = ", error!! ;)";
+}
+userNick += userMessage;
+console.log(userNick);
+
+// 
+if (55 > 10) {
+   console.log("Hello world");
+} else {
+   console.log("Hello hell :^)");
+}
